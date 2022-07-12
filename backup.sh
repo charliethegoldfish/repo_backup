a20f183a1e8c8e2b6045b16771b53fb521ba77f4
Charlie Francis Cassidy <charliefrancis.cassidy@gmail.com>
2022-07-12 18:31:15 +1000

    Basic structure for backup script
    Backup script with comments on basic structure of how it should work

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the directory we want to backup into
+# Perhaps assume it's the one we are running our script from?
+
+# Get list of repo's from somewhere
+# Defined in a txt file in the directory we're using?
+# repos.txt
+
+# Loop through repo list
+# If not checked out, check out
+# If checked out, pull down latest
+
+# Wait for x time before checking if there is new stuff to pull down
+
+# Repeat above loop
